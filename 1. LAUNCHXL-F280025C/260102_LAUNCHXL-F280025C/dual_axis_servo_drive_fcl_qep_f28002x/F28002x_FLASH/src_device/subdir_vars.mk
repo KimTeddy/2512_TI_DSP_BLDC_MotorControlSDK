b070a207e2a26df45e666dd343d8e020f003fdb6
TeddyElectronics <kyc20126@gmail.com>
2026-01-02 14:55:51 +0900

    260102_LAUNCHXL-F280025C

---
+++ 1. LAUNCHXL-F280025C/260102_LAUNCHXL-F280025C/dual_axis_servo_drive_fcl_qep_f28002x/F28002x_FLASH/src_device/subdir_vars.mk
@@ -0,0 +1,50 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+C:/ti/C2000Ware_MotorControl_SDK_5_04_00_00/solutions/boostxl_3phganinv/f28002x/cmd/dual_axis_f28002x_flash_lnk.cmd \
+C:/ti/C2000Ware_MotorControl_SDK_5_04_00_00/solutions/boostxl_3phganinv/f28002x/cmd/f28002x_headers_nonbios_eabi.cmd 
+
+ASM_SRCS += \
+C:/ti/C2000Ware_MotorControl_SDK_5_04_00_00/c2000ware/device_support/f28002x/common/source/f28002x_codestartbranch.asm 
+
+C_SRCS += \
+C:/ti/C2000Ware_MotorControl_SDK_5_04_00_00/c2000ware/device_support/f28002x/common/source/device.c \
+C:/ti/C2000Ware_MotorControl_SDK_5_04_00_00/c2000ware/device_support/f28002x/headers/source/f28002x_globalvariabledefs.c 
+
+C_DEPS += \
+./src_device/device.d \
+./src_device/f28002x_globalvariabledefs.d 
+
+OBJS += \
+./src_device/device.obj \
+./src_device/f28002x_codestartbranch.obj \
+./src_device/f28002x_globalvariabledefs.obj 
+
+ASM_DEPS += \
+./src_device/f28002x_codestartbranch.d 
+
+OBJS__QUOTED += \
+"src_device\device.obj" \
+"src_device\f28002x_codestartbranch.obj" \
+"src_device\f28002x_globalvariabledefs.obj" 
+
+C_DEPS__QUOTED += \
+"src_device\device.d" \
+"src_device\f28002x_globalvariabledefs.d" 
+
+ASM_DEPS__QUOTED += \
+"src_device\f28002x_codestartbranch.d" 
+
+C_SRCS__QUOTED += \
+"C:/ti/C2000Ware_MotorControl_SDK_5_04_00_00/c2000ware/device_support/f28002x/common/source/device.c" \
+"C:/ti/C2000Ware_MotorControl_SDK_5_04_00_00/c2000ware/device_support/f28002x/headers/source/f28002x_globalvariabledefs.c" 
+
+ASM_SRCS__QUOTED += \
+"C:/ti/C2000Ware_MotorControl_SDK_5_04_00_00/c2000ware/device_support/f28002x/common/source/f28002x_codestartbranch.asm" 
+
+
